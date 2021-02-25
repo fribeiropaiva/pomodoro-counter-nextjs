63fa578d41e95eba88fdd462c9b5ccb49e4afdf3
Filipe Ribeiro <filipepaiva240@gmail.com>
2021-02-25 06:18:56 -0300

    Migrate project to next js

---
+++ components/ExperienceBar.js
@@ -0,0 +1,14 @@
+import styles from './ExperienceBar.module.css';
+
+export function ExperienceBar(props) {
+  return (
+    <div className={styles.ExperienceBarContainer}>
+      <span>0xp</span>
+      <div className={styles.Bar}>
+        <div className={styles.BarFill}></div>
+        <span className={styles.CurrentXP}>300xp</span>
+      </div>
+      <span>600xp</span>
+    </div>
+  )
+}
